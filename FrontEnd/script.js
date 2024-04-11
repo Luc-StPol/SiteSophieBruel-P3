b7b06e502d27763a58431290fd41c62c2762f02c
Luc-StPol <saintpol.luc@gmail.com>
2024-04-11 09:59:44 +0200

    Ajout des filtres pour les catégories de travaux

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -2,22 +2,47 @@
 
 
 async function fetchWorks() {
+
     const response = await fetch("http://localhost:5678/api/works")
     const dataResponse = await response.json()
 
-    const galleryDiv = document.getElementById("gallery")
+    let category = 0
     
-    for(let i=0; i < dataResponse.length; i++) {
-        const figureElement = document.createElement("figure")
-        figureElement.innerHTML = `
-        <img src="${dataResponse[i].imageUrl}" alt="${dataResponse[i].title}">
-        <figcaption> ${dataResponse[i].title} </figcaption>`
-         galleryDiv.appendChild(figureElement)
-        
-    }
+    const filtreForm = document.getElementById("filtreForm")
+
+    filtreForm.addEventListener("change", (e) => {
+        category = e.target.value
+        filtre(category, dataResponse)
+    
+    })
+
+filtre(category, dataResponse)        
 
 }
 
+function filtre(category, dataResponse ) {
+
+    const galleryDiv = document.getElementById("gallery")
+    
+    let worksData = dataResponse
+
+    galleryDiv.innerHTML=""
+
+        if (category == 0) {
+            worksData = dataResponse
+        } else {
+            worksData = dataResponse.filter((data) => data.categoryId == category)
+        }
+
+        for(let i=0; i < worksData.length; i++) {
+            const figureElement = document.createElement("figure")
+            figureElement.innerHTML = `
+            <img src="${worksData[i].imageUrl}" alt="${worksData[i].title}">
+            <figcaption> ${worksData[i].title} </figcaption>`
+            galleryDiv.appendChild(figureElement)
+            
+        }
+    }
 
 fetchWorks()
 
